744d06a6d795c471d60613a5ca5c98a37f59a663
prig25 <79767153+prig25@users.noreply.github.com>
2021-03-14 19:22:30 +0300

    feat: add code of solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) {return a - b});
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) {return b - a;});
+    return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    for (var i=0; i<array.length; i++) {
+      sum = sum + array[i];
+    }
+    return sum/array.length;
+  }  
 }
